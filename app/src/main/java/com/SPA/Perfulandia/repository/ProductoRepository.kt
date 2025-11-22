929e5816430a317f008f07ec5815e0f435a9cc36
EduDuoc <ed.delgadillo@duocuc.cl>
2025-11-21 23:18:44 -0300

    edicion de texto validacion

--- app/src/main/java/com/SPA/Perfulandia/repository/ProductoRepository.kt
+++ app/src/main/java/com/SPA/Perfulandia/repository/ProductoRepository.kt
@@ -7,20 +7,13 @@ import kotlinx.coroutines.flow.Flow
 class ProductoRepository(
     private val dao: ProductoDao
 ){
-
     val productos: Flow<List<Producto>> = dao.getAll()
 
-    suspend fun insertar(producto: Producto) {
-        dao.insert(producto)
-    }
+    suspend fun insertar(producto: Producto) {dao.insert(producto)}
 
-    suspend fun eliminar(producto: Producto) {
-        dao.delete(producto)
-    }
+    suspend fun eliminar(producto: Producto) {dao.delete(producto)}
 
-    suspend fun actualizar(producto: Producto) {
-        dao.update(producto)
-    }
+    suspend fun actualizar(producto: Producto) {dao.update(producto)}
 
     suspend fun obtenerPorId(id: Int): Producto? {
         return dao.getById(id)
